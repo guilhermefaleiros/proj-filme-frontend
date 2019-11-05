0293f988ba5b58c532c98619dcf8154dff97ca86
Guilherme Faleiros <guilhermefaleiros2000@gmail.com>
2019-11-04 21:36:08 -0300

    Alterações na página de busca

--- src/pages/Busca/index.js
+++ src/pages/Busca/index.js
@@ -1,10 +1,21 @@
-import React, {useState} from 'react'
-
+import React, {useState, useEffect} from 'react'
+import api from '../../services/api'
 import './styles1.css'
 
 export default function Busca({history}){
     const [filtro, setFiltro] = useState('')
-
+    const [movies, setMovies] = useState([])
+    useEffect(() => { 
+        
+      async function loadMovies(){
+          
+          const response = await api.get('/dashboard')
+          setMovies(response.data)
+          console.log(response.data)
+      }
+      loadMovies()
+      
+  }, [])
     async function handleSubmit(event){
         event.preventDefault()
         const str = filtro.toLowerCase()
@@ -23,16 +34,41 @@ export default function Busca({history}){
         </p>
 
         <form onSubmit={handleSubmit}>
-          <label htmlFor="filtro">DIGITE O GÊNERO DO FILME DESEJADO</label>
-          <input type="text"
-           id="filtro"
-            placeholder="Ex: DRAMA, AÇÃO, etc..."
-            value={filtro}
-            onChange={event => setFiltro(event.target.value)}/>
-          <button className="btn" type="submit">
+          <label htmlFor="filtro">SELECIONE O GÊNERO DO FILME DESEJADO</label>
+           <select name="" id="filtro" value={filtro}
+            onChange={event => setFiltro(event.target.value)}>
+             <option value="Drama">Drama</option>
+             <option value="Ação">Ação</option>
+             <option value="Romance">Romance</option>
+             <option value="Terror">Terror</option>
+             <option value="Histórico">Histórico</option>
+             <option value="Guerra">Guerra</option>
+             <option value="Documentário">Documentário</option>
+             <option value="Thriller">Thriller</option>
+             <option value="Comédia">Comédia</option>
+             <option value="Aventura">Aventura</option>
+             <option value="Animação">Animação</option>
+             <option value="Família">Família</option>
+             <option value="Ficção Científica">Ficção Científica</option>
+             <option value="Biografia">Biografia</option>
+             <option value="Crime">Crime</option>
+             <option value="Mistério">Mistério</option>
+           </select>
+            <button className="btn" type="submit">
             Buscar
           </button>
         </form>
+        <ul className="movie-list">
+                {movies.map(movie => (
+                    <li key={movie._id}>
+                        <header style={{ backgroundImage: `url(${movie.thumbnail})` }}/>
+                        <strong>{movie.title}</strong>
+                        <span>Nota IMDB: {movie.score}</span>
+                        <span>N° IMDB: {movie.id}</span>
+                        <span><a href={`http://www.imdb.com/title/tt0${movie.id}`}>Clique para acessar no IMDB</a></span>
+                    </li>
+                ))}
+            </ul>
         </div>
     </div>
     )
